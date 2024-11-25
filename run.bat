9be3aca2e3c8765e24791fb1a1267b36e87a6654
Unnoticeableatall26 <83273080+Unnoticeableatall26@users.noreply.github.com>
2024-11-25 11:42:38 +0100

    Update run.bat
    2 and 3

--- run.bat
+++ run.bat
@@ -1,13 +1,13 @@
 @echo off
-REM Replace "index.html" with the actual filename if it's different
-set filename=index.html
+REM Set the HTML file name
+set filename=DevoirTODOList.html
 
 REM Check if the file exists
 if exist "%filename%" (
-    REM Open the HTML file in the default web browser
-    start "" "%filename%"
-    echo Launching %filename% in your default web browser...
+    REM Open the HTML file in the default web browser
+    start "" "%filename%"
+    echo Launching %filename% in your default web browser...
 ) else (
-    echo File "%filename%" not found!
-    pause
+    echo File "%filename%" not found!
+    pause
 )
